5d5e9706368abb3e4dc108c8721cef29e012f367
Vnicolas <victor.nicolasc@um.es>
2023-03-27 15:42:47 +0200

    Remove several prints from scalability plot

--- scalabilityPlot.R
+++ scalabilityPlot.R
@@ -17,22 +17,27 @@ currArg <- 8
 # Variable arguments start
 
 n_lines_configs <- as.integer(arguments[currArg])
+n_lines_configs <- n_lines_configs - 1
+
 currArg <- increment(currArg)
 n_stats <- as.integer(arguments[currArg])
-n_stats <- n_stats - 1
+
 currArg <- increment(currArg)
-if (n_stats > 1) {
+if (n_stats != 1) {
     stop("Scalability plot not implemented with more than one stat")
 }
 line_stat <- NULL
 for (i in 1:n_stats) {
   line_stat <- c(line_stat, arguments[currArg])
   currArg <- increment(currArg)
+
 }
 x_config <- arguments[currArg]
+
 currArg <- increment(currArg)
 var_iterate <- arguments[currArg]
 
+currArg <- increment(currArg)
 lines_names <- NULL
 n_lines_names <- arguments[currArg]
 currArg <- increment(currArg)
@@ -50,17 +55,22 @@ if (n_lines_names > 0) {
 parsed_data <- read.table(fileName, sep = " ", header=TRUE)
 
 # Prepare Y-axis (pick stats names)
+parsed_data["confLine"] <- ""
 parsed_data["confLine"] <- mixStringCols(1, n_lines_configs, parsed_data)
+
 # To keep the order from the configs, turn them into a factor
 parsed_data$confLine <- factor(parsed_data$confKey, levels = unique(as.character(parsed_data$confKey)), ordered = TRUE)
 # Do not iterate over duplicates
+
 vars <- unique(parsed_data[,var_iterate])
+
 # Iterate over selected variable
 for (var in vars) {
-    plot_data <- parsed_data[parsed_data[var_iterate] == var]
-p <- ggplot(data=plot_data, aes(x=plot_data[,x_config], y=plot_data[,line_stat], group=plot_data[,"confLine"])) +
-    geom_line(aes(color=plot_data[,"confLine"])) +
-    geom_point(aes(color=plot_data[,"confLine"]))
-    ggsave(paste(c(plot.filename, "_", var, ".jpg"), collapse = ""), width=plot.width, height=plot.height, units="cm", dpi=320, device="jpg")
+    plot_data <- datos <- data.frame(x_config = character(), line_stat=numeric(), confLine=character(), stringsAsFactors = FALSE)
+    plot_data <- as.data.frame(parsed_data[parsed_data[var_iterate] == var])
+p <- ggplot(data=parsed_data, aes(x=x_config, y=line_stat, group=parsed_data[,"confLine"])) +
+    geom_line() +
+    geom_point()
+    ggsave(paste(c(plot.fileName, "_", var, ".jpg"), collapse = ""), width=plot.width, height=plot.height, units="cm", dpi=320, device="jpg")
 }
 
