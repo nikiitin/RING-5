dbcf830fdb029d8a1ffe6c810947484a56a1d066
Victor Nicolas Conesa <victor.nicolasc@um.es>
2023-12-08 23:46:49 +0100

    Refine seeds reduction script

--- data_management/data_manager_R/reduceSeeds.R
+++ data_management/data_manager_R/reduceSeeds.R
@@ -5,16 +5,14 @@ source("utils/util.R")
 
 arguments <- commandArgs(trailingOnly = TRUE)
 stats_file <- arguments[1]
-configs <- arguments[2]
-configs <- as.numeric(configs)
 parsed_data <- read.table(stats_file, sep = " ", header = TRUE)
+
 # Check if random_seed column exists
-if (!("random_seed" %in% colnames(parsed_data))) {
+if (!check_column_exists("random_seed", parsed_data)) {
     stop("random_seed column does not exist! Not reducing seeds!")
 }
-seeds_column <- which(colnames(parsed_data) == "random_seed")
-# Prepare the csv to be parsed by plotters
-# Calculate mean
+seeds_column <- get_column_index("random_seed", parsed_data)
+
 # All columns from seeds_column to the end should be statistics
 # so we should convert them to numeric and do not get any error
 # when calculating the mean. Check if the column can be converted
@@ -24,9 +22,11 @@ if (!all(
 ) {
     stop("Statistics found not numeric! Not reducing seeds!")
 }
+
 # Convert all columns to numeric from seeds_column to the end
 parsed_data[(seeds_column + 1):ncol(parsed_data)] <-
     sapply(parsed_data[(seeds_column + 1):ncol(parsed_data)], as.numeric)
+
 # Calculate mean for all statistics
 mean_dataframe <- aggregate(
     parsed_data[(seeds_column + 1):ncol(parsed_data)],
@@ -40,6 +40,7 @@ sd_dataframe <- aggregate(
     by = parsed_data[1:seeds_column],
     FUN = sd)
 sd_dataframe["random_seed"] <- NULL
+
 # Rename sd columns to sd-<column_name>
 colnames(sd_dataframe)[(seeds_column + 1):ncol(sd_dataframe)] <-
     paste("sd",
