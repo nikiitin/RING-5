294bc63b0b414c84fac77206ae593b27db3b996d
Victor Nicolas Conesa <victor.nicolasc@um.es>
2023-12-12 16:37:27 +0100

    Simplify outliers removal

--- data_management/data_manager_R/removeOutliers.R
+++ data_management/data_manager_R/removeOutliers.R
@@ -19,27 +19,27 @@ if (!check_column_exists("random_seed", parsed_data)) {
     stop("random_seed column does not exist. No outliers to remove!")
 }
 n_configs <- get_column_index("random_seed", parsed_data)
-# Make the config keys to address every configuration
+# ConfKey column should already contain benchmark_name and
+# configurations
 for (conf in unique(parsed_data[, "confKey"])) {
-    for (bench in unique(parsed_data[, "benchmark_name"])) {
-        data_for_outlier <- parsed_data[parsed_data["benchmark_name"] == bench &
-            parsed_data["confKey"] == conf,
-            outlier_stat]
-        # Calculate quantile and IQR (Using IQR between mean and first element)
-        # as we are removing only upper outliers (System calls, page faults...)
-        q <- quantile(data_for_outlier, probs=c(0, 0.5), na.rm = FALSE)
-        iqr <- q[2] - q[1]
-        # Set the threshold
-        up <-  q[2] + iqr
-        # Remove outliers by letting only values that lie below the threshold
-        # or the ones that are not from the current configuration
-        parsed_data <- subset(parsed_data,
-            (parsed_data["benchmark_name"] != bench |
-                parsed_data["confKey"] != conf) |
-            ((parsed_data["benchmark_name"] == bench &
-                parsed_data["confKey"] == conf) &
-                parsed_data[outlier_stat] < up))
-    }
+    data_for_outlier <- parsed_data[
+        parsed_data["confKey"] == conf,
+        outlier_stat
+    ]
+    # Calculate quantile and IQR (Using IQR between mean and first element)
+    # as we are removing only upper outliers (System calls, page faults...)
+    q <- quantile(data_for_outlier, probs = c(0, 0.5), na.rm = FALSE)
+    iqr <- q[2] - q[1]
+    # Set the threshold
+    up <- q[2] + iqr
+    # Remove outliers by letting only values that lie below the threshold
+    # or the ones that are not from the current configuration
+    parsed_data <- subset(
+        parsed_data,
+        (parsed_data["confKey"] != conf) |
+            (parsed_data["confKey"] == conf &
+                parsed_data[outlier_stat] < up)
+    )
 }
 # Write filtered data onto csv file
 write.table(parsed_data, stats_file, sep = " ", row.names = FALSE)
